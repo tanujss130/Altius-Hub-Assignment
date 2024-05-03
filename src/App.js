135718bc6fc3b915602ffcc4d78985e1065dc2d0
Tanuj sharma <tanuj.sharma@napierhealthcare.com>
2024-05-03 19:29:08 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import './App.css';
+// App.js
+import React from 'react';
 import ToolbarComponent from './components/toolbar/Toolbar';
 import SideNav from './components/side-nav-bar/SideNav';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ListView from './components/list-view/ListView';
+import { Container, Grid } from '@mui/material';
+
 function App() {
   return (
     <Router>
-      <div>
-        <ToolbarComponent />
-        <SideNav />
-        <Routes>
-          <Route path='/' />
-          <Route path='/listView' element={<ListView />} />
-        </Routes>
-      </div>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <ToolbarComponent />
+          </Grid>
+          <Grid item xs={3}>
+            <SideNav />
+          </Grid>
+          <Grid item xs={12}>
+            <Routes>
+              <Route path='/' />
+              <Route path='/listView' element={<ListView />} />
+            </Routes>
+          </Grid>
+        </Grid>
+      </Container>
     </Router>
   );
 }
